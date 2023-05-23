bdb0196fdacc5057d3f662d378ec9bfadfc7cc02
danduezafdam1b <113137502+danduezafdam1b@users.noreply.github.com>
2023-05-23 18:32:15 +0200

    Corregido

--- texto/Pruba.java
+++ texto/Pruba.java
@@ -1,7 +1,9 @@
 package texto;
 
 public class Pruba {
-    system.out.println("Hola soy People of Tomorrow");
-    system.out.println("Adios People of Tomorrow, Hola soy Paco Torres");
-    system.out.println("Estoy en un high mood");
+    public void texto(){
+        System.out.println("Hola soy People of Tomorrow");
+        System.out.println("Adios People of Tomorrow, Hola soy Paco Torres");
+        System.out.println("Estoy en un high mood");
+    }
 }
