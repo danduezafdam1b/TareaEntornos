3bee5c6fa5b35f22148c136a9693d6b55d589650
inAHighMood <134420523+inAHighMood@users.noreply.github.com>
2023-05-23 18:22:48 +0200

    commit de inAHighMood

--- texto/Pruba.java
+++ texto/Pruba.java
@@ -3,4 +3,5 @@ package texto;
 public class Pruba {
     system.out.println("Hola soy People of Tomorrow");
     system.out.println("Adios People of Tomorrow, Hola soy Paco Torres");
+    system.out.println("Estoy en un high mood");
 }
